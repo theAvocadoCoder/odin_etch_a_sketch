e96be0bce00c52e66de469a4570fe2ce66767ced
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-01-30 03:41:28 +0100

    feat: Improve pen function and color picker
    - Add color input element to allow for broader range of color options
    - Edit event listeners so user can click to draw on desktop

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,6 @@
 /* Variables */
 
-const colorPicker = document.querySelector(".color-picker"),
-  colors = document.querySelectorAll(".color-div"),
-  root = document.getElementById("root"),
+const root = document.getElementById("root"),
   uiContainer = document.querySelector(".ui-container");
 
 const eight = document.getElementById("eight"),
@@ -11,55 +9,47 @@ const eight = document.getElementById("eight"),
   sixtyFour = document.getElementById("sixty-four");
 
 const clearBtn = document.getElementById("clear-btn"),
-  pickColor = document.getElementById("pick-color");
+  colorPicker = document.getElementById("color-picker"),
+  pickColor = document.getElementById("pick-color"),
+  randomColorBtn = document.getElementById("random-color");
+
+let gridDivs = [];
 
 let globalGridValue = 16;
 
-const RED = '#ff0000',
-  ORANGE = '#ffa500',
-  YELLOW = '#ffff00',
-  GREEN = '#00ff00',
-  BLUE = '#0000ff',
-  INDIGO = '#4b0082',
-  VIOLET = '#ee82ee',
-  CLEAR = '#fafaff',
-  DEFAULT = '#000030',
-  GRAY = '#808080',
-  BLACK = '#000000';
+let penUp = true;
 
-const COLORS = {
-  RED,
-  ORANGE,
-  YELLOW,
-  GREEN,
-  BLUE,
-  INDIGO,
-  VIOLET,
-  CLEAR,
-  DEFAULT,
-  GRAY,
-  BLACK,
-  RANDOM: function () { return randomColor() }
-};
-
-let chosenColor = DEFAULT;
+let chosenColor = "#000";
 
 
 
 /* Event Listeners */
 
-window.addEventListener('DOMContentLoaded', populate(16));
+window.addEventListener("DOMContentLoaded", () => {
+  populate(16);
 
-eight.addEventListener('click', () => clearGrid(8, 8));
-sixteen.addEventListener('click', () => clearGrid(16, 16));
-thirtyTwo.addEventListener('click', () => clearGrid(32, 32));
-sixtyFour.addEventListener('click', () => clearGrid(64, 64));
+  const draw = (e) => penTool(e.target);
 
-clearBtn.addEventListener('click', () => clearGrid(globalGridValue, globalGridValue));
-pickColor.addEventListener('click', () => {
-  colorPicker.style.display = "flex";
-  root.style.display = "none";
+  root.addEventListener("click", () => {
+    gridDivs.forEach(div => {
+      if (penUp) {
+        div.addEventListener("mouseover", draw)
+      } else {
+        div.removeEventListener("mouseover", draw)
+      }
+    })
+    penUp = !penUp;
+  })
 });
+
+eight.addEventListener("click", () => clearGrid(8));
+sixteen.addEventListener("click", () => clearGrid(16));
+thirtyTwo.addEventListener("click", () => clearGrid(32));
+sixtyFour.addEventListener("click", () => clearGrid(64));
+
+clearBtn.addEventListener("click", () => clearGrid(globalGridValue, globalGridValue));
+randomColorBtn.addEventListener("click", () => chosenColor = randomColor());
+colorPicker.addEventListener("input", (e) => chosenColor = e.target.value);
 
 
 
@@ -72,78 +62,28 @@ function penTool(div) {
 function populate(grids) {
   for (let i = 1; i <= (grids * grids); i++) {
     let newDiv = document.createElement("div");
+    newDiv.classList.add("grid-div");
     newDiv.id = `grid-div-${i}`;
-    newDiv.style.backgroundColor = CLEAR;
+    newDiv.style.backgroundColor = "#fff";
     newDiv.style.width = `${100 / grids}%`;
     newDiv.style.height = newDiv.style.width;
     root.appendChild(newDiv);
-    newDiv.addEventListener('mouseover', () => penTool(newDiv));
   }
-
-  colors.forEach(color => {
-    if (color.id === 'random') {
-      color.style.backgroundColor = 'black';
-    } else {
-      color.style.backgroundColor = COLORS[color.id.toUpperCase()];
-    }
-    color.addEventListener('click', () => chooseColor(color.id));
-  })
+  gridDivs = Array.from(document.getElementsByClassName("grid-div"));
 }
 
-function clearGrid(grids, newGlobalGridValue) {
-  root.innerHTML = '';
-  globalGridValue = newGlobalGridValue;
+function clearGrid(grids) {
+  root.innerHTML = "";
+  globalGridValue = grids;
   populate(grids);
 }
 
-function chooseColor(color) {
-
-  switch (color.toUpperCase()) {
-    case "RED":
-      chosenColor = COLORS.RED;
-      break;
-    case "ORANGE":
-      chosenColor = COLORS.ORANGE;
-      break;
-    case "YELLOW":
-      chosenColor = COLORS.YELLOW;
-      break;
-    case "GREEN":
-      chosenColor = COLORS.GREEN;
-      break;
-    case "BLUE":
-      chosenColor = COLORS.BLUE;
-      break;
-    case "INDIGO":
-      chosenColor = COLORS.INDIGO;
-      break;
-    case "VIOLET":
-      chosenColor = COLORS.VIOLET;
-      break;
-    case "CLEAR":
-      chosenColor = COLORS.CLEAR;
-      break;
-    case "DEFAULT":
-      chosenColor = COLORS.DEFAULT;
-      break;
-    case "GRAY":
-      chosenColor = COLORS.GRAY;
-      break;
-    case "BLACK":
-      chosenColor = COLORS.BLACK;
-      break;
-    case "RANDOM":
-      chosenColor = COLORS.RANDOM();
-      break;
-    default:
-      chosenColor = DEFAULT;
-  }
-
-  colorPicker.style.display = "none";
-  root.style.display = "flex";
-}
-
 function randomColor() {
-  const arr = [RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET, CLEAR, DEFAULT, GRAY, BLACK];
-  return arr[Math.floor(Math.random() * arr.length)];
+  const arr = ["0", "1", "2", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+  let randomVal = () => Math.floor(Math.random() * arr.length);
+  const hexVals = [];
+  for (let i = 0; i < 6; i++) {
+    hexVals.push(arr[randomVal()]);
+  }
+  return "#" + hexVals.join("");
 }
